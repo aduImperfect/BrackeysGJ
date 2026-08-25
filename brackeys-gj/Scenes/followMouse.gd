167213c23595dbdd6831776eaae8d44e928fa5d5
Kowshik <saikowsikayyalasomayajula@gmail.com>
2026-08-25 20:12:03 +0530

    ui elements mouse follow on click

---
+++ brackeys-gj/Scenes/followMouse.gd
@@ -0,0 +1,18 @@
+extends TextureRect
+
+var is_dragging: bool = false
+var drag_offset: Vector2 = Vector2.ZERO
+
+func _process(_delta: float) -> void:
+	if is_dragging:
+		global_position = get_global_mouse_position() - drag_offset
+
+# godot callback
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			is_dragging = true
+			drag_offset = get_global_mouse_position() - global_position
+			
+		elif event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			is_dragging = false
