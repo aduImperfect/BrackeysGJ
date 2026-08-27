0cac4b0d085639a70fe953ab720f9b130480c2d9
Kowshik <saikowsikayyalasomayajula@gmail.com>
2026-08-28 03:02:44 +0530

    added evidenceEntries

---
+++ brackeys-gj/Scripts/evidenceEntry.gd
@@ -0,0 +1,17 @@
+class_name EvidenceEntry
+
+extends Resource
+
+@export var evidence_name: String = ""
+@export var characters: Array[String] = []
+@export var location: String = ""
+@export var time: String = ""
+@export_multiline var notes: String = ""
+
+func makeEntry(p_name: String, p_characters: Array[String], p_location: String, p_time: String) -> EvidenceEntry:
+	var e := EvidenceEntry.new()
+	e.evidence_name = p_name
+	e.characters = p_characters
+	e.location = p_location
+	e.time = p_time
+	return e
