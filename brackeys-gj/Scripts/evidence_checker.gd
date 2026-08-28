5a5c7433b167d91d67a67d978532f8107c1e611b
Kowshik <saikowsikayyalasomayajula@gmail.com>
2026-08-28 16:51:20 +0530

    modified and added evidenceChecker

--- brackeys-gj/Scripts/evidence_checker.gd
+++ brackeys-gj/Scripts/evidence_checker.gd
@@ -1,12 +1,14 @@
-extends Resource
+extends Node2D
 
-@export var entries: Array[EvidenceEntry] = []
+@export var validEvidencesRes : Resource
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func _checkEvidence() -> void:
 	pass
