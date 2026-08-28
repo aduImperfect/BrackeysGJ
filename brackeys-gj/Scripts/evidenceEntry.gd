a39c6abdc679ac3c33c389256f623ea6ef3be923
Kowshik <saikowsikayyalasomayajula@gmail.com>
2026-08-28 18:19:39 +0530

    checking evidences

--- brackeys-gj/Scripts/evidenceEntry.gd
+++ brackeys-gj/Scripts/evidenceEntry.gd
@@ -8,10 +8,16 @@ extends Resource
 @export var time: String = ""
 @export_multiline var notes: String = ""
 
-func makeEntry(p_name: String, p_characters: Array[String], p_location: String, p_time: String) -> EvidenceEntry:
+static func makeEntry(p_name: String, p_characters: Array[String], p_location: String, p_time: String) -> EvidenceEntry:
 	var e := EvidenceEntry.new()
 	e.evidence_name = p_name
 	e.characters = p_characters
 	e.location = p_location
 	e.time = p_time
 	return e
+
+static func compareEntries(a: EvidenceEntry, b: EvidenceEntry) -> bool:
+	return a.evidence_name == b.evidence_name \
+		and a.location == b.location \
+		and a.time == b.time \
+		and a.characters == b.characters
--- brackeys-gj/Scripts/evidence_checker.gd
+++ brackeys-gj/Scripts/evidence_checker.gd
@@ -2,15 +2,23 @@ extends Node2D
 
 class_name  EvidenceChecker
 
-@export var validEvidencesRes : Resource
+@export var validEvidencesRes : AllEvidences
 
+static var staticValidEvidences : AllEvidences
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	staticValidEvidences = validEvidencesRes
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-static func _checkEvidence(_playerEvidenceRes : Resource) -> void:
-	pass
+static func _checkEvidence(_playerEvidenceRes : AllEvidences) -> bool:
+	var alright : bool = true
+	for i in _playerEvidenceRes.entries.size():
+		var playerEntry = _playerEvidenceRes.entries[i]
+		if(!EvidenceEntry.compareEntries(playerEntry, staticValidEvidences.entries[i])):
+			alright = false
+			break
+	return alright
