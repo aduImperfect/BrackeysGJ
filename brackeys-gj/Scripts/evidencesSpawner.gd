a974f28344c27c1f5d987581c0f5cd6cbda92bac
Aditya Subramanian <adu101992@gmail.com>
2026-08-29 18:28:09 +0530

    Scrolling and positioning of the evidences updated.

--- brackeys-gj/Scripts/evidencesSpawner.gd
+++ brackeys-gj/Scripts/evidencesSpawner.gd
@@ -5,6 +5,12 @@ var evidenceScene : PackedScene = preload("res://Scenes/evidence_template.tscn")
 @export var holder : Control
 @export var hOffset : float = 150
 @export var vOffset : float = 100
+@export var minxOffset : float = 0
+@export var minyOffset : float = 0
+@export var scrollMultiplier : int = 0
+
+@export var evidenceLocker : Control
+
 var spawnedEvidences : Array[Control]
 
 # Called when the node enters the scene tree for the first time.
@@ -12,13 +18,16 @@ func _ready() -> void:
 	spawnEvidences()
 
 func spawnEvidences() -> void:
+	#Make sure to expand the scroll size depending on the number of evidences spawned in.
+	evidenceLocker.custom_minimum_size.y = scrollMultiplier * (evidenceData.entries.size()/2)
+
 	for evidenceIndex in  evidenceData.entries.size():
 		var spawnedEvidence = evidenceScene.instantiate() as EvidenceData
 		var column = evidenceIndex % 2
 		var row = evidenceIndex / 2
 		var spawnPos = global_position + Vector2(
-			column * hOffset,
-			row * vOffset
+			minxOffset + column * hOffset,
+			minyOffset + row * vOffset
 		)
 		holder.add_child(spawnedEvidence)
 		spawnedEvidence.name = "Evidence" +  str(spawnedEvidences.size() + 1)
@@ -27,5 +36,5 @@ func spawnEvidences() -> void:
 		print(str(evidenceIndex))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
