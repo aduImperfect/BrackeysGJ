0c8580374147238e7160430ceaeb86ad7b18c6c9
Aditya Subramanian <adu101992@gmail.com>
2026-08-30 01:13:40 +0530

    Shifted some fix with the evidence list UI

--- brackeys-gj/Scripts/evidence_header.gd
+++ brackeys-gj/Scripts/evidence_header.gd
@@ -1,14 +1,14 @@
 extends TextEdit
 
+static var startingPos : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	startingPos = global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	global_position = startingPos
 
 func _input(event: InputEvent):
 	# Check if a mouse button is clicked while the node has focus
