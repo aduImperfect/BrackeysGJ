74b920dbe44322514ea70c5260bd1583cc89c722
Aditya Subramanian <adu101992@gmail.com>
2026-08-30 00:59:57 +0530

    Timeline scroll fix.

--- brackeys-gj/Scripts/timeline_scroll_container.gd
+++ brackeys-gj/Scripts/timeline_scroll_container.gd
@@ -4,11 +4,17 @@ extends ScrollContainer
 
 @export var evidenceTimeline : Control
 
+@export var priorEvidencesSize : int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	priorEvidencesSize = ChainOfEvidence.spawnedEvidencesEntries.size()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	#Make sure to expand the scroll size depending on the number of evidences in the chain.
-	evidenceTimeline.custom_minimum_size.x = scrollMultiplier * ChainOfEvidence.spawnedEvidencesEntries.size()
+	if priorEvidencesSize != ChainOfEvidence.spawnedEvidencesEntries.size():
+		print("Prior Evidences Size: ", priorEvidencesSize)
+		#Make sure to expand the scroll size depending on the number of evidences in the chain.
+		evidenceTimeline.custom_minimum_size.x = scrollMultiplier * ChainOfEvidence.spawnedEvidencesEntries.size()
+		print("Custom Min Size X: ", evidenceTimeline.custom_minimum_size.x)
+		priorEvidencesSize = ChainOfEvidence.spawnedEvidencesEntries.size()
