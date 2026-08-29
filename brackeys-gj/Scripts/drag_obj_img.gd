72f5a01b70b4831389ae9105893caef7306118d6
Aditya Subramanian <adu101992@gmail.com>
2026-08-29 18:32:14 +0530

    The draggable texture object now also correctly updates as intended!

--- brackeys-gj/Scripts/drag_obj_img.gd
+++ brackeys-gj/Scripts/drag_obj_img.gd
@@ -2,11 +2,15 @@ extends Sprite2D
 
 class_name DragObjImage
 
+var newTex : Texture2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	texture = newTex
+
+func _update_texture(_texVal : Texture2D) -> void:
+	newTex = _texVal
