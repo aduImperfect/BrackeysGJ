f0953880f98837c5d5802281e6d4d034a30f3ed3
Kowshik <saikowsikayyalasomayajula@gmail.com>
2026-08-30 01:14:51 +0530

    line for chain of evidences

--- brackeys-gj/Scripts/chainOfEvidence.gd
+++ brackeys-gj/Scripts/chainOfEvidence.gd
@@ -1,50 +1,62 @@
-extends Node
-
+extends Control
 class_name ChainOfEvidence
 
 @export var itemsHolder : Control
 @export var itemInfo : PackedScene
-static var offset : float = 300
+@export var lineColor : Color = Color.WHITE
+@export var lineWidth : float = 2.0
+@export var startOffset : Vector2 = Vector2.ZERO
+@export var endOffset : Vector2 = Vector2.ZERO
 
+static var offset : float = 300
 var validEvidences : ValidEvidences = preload("res://Resources/evidencesRes.tres")
 static var itemsHolderInternal : Control
 static var itemInfoInternal : PackedScene
 static var spawnedEvidences : Array[Node]
 static var spawnedEvidencesEntries : Array[EvidenceEntry]
+static var selfInternal : ChainOfEvidence
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	itemsHolderInternal = itemsHolder
 	itemInfoInternal = itemInfo
+	selfInternal = self
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if spawnedEvidences.size() == validEvidences.entries.size():
 		EvidenceCheck.enableButton = true
 	else:
 		EvidenceCheck.enableButton = false
-
 	if TimelineReset.resetTimeline:
 		_clearEvidences()
 		TimelineReset.resetTimeline = false
 
+func _draw() -> void:
+	var xform := get_global_transform().affine_inverse()
+	for i in range(spawnedEvidences.size() - 1):
+		var la := spawnedEvidences[i] as Node2D
+		var lb := spawnedEvidences[i + 1] as Node2D
+		if not is_instance_valid(la) or not is_instance_valid(lb):
+			continue
+		var from := xform * la.global_position + startOffset
+		var to := xform * lb.global_position + endOffset
+		draw_line(from, to, lineColor, lineWidth)
+
 static func _spawnEvidence(entry: EvidenceEntry) -> void:
 	var newEvidence := itemInfoInternal.instantiate() as Node2D
-
 	var spawnPos := itemsHolderInternal.global_position
 	spawnPos.x += spawnedEvidences.size() * offset
-
 	itemsHolderInternal.add_child(newEvidence)
 	newEvidence.global_position = spawnPos
-
 	var nameLab := newEvidence.find_child("EvidenceName") as Label
 	nameLab.text = entry.evidence_name
-	
+
 	var evidenceTex := newEvidence.find_child("EvidenceTex") as TextureRect
 	evidenceTex.texture = entry.evidence_tex
-
 	spawnedEvidencesEntries.append(entry)
 	spawnedEvidences.append(newEvidence)
+
+	if selfInternal:
+		selfInternal.queue_redraw()
 
 func _checkAndPopulateEvidences(_playerEvidences : AllEvidences) -> void:
 	_clearEvidences()
@@ -56,3 +68,4 @@ func _clearEvidences() -> void:
 		evidence.queue_free()
 	spawnedEvidences.clear()
 	spawnedEvidencesEntries.clear()
+	queue_redraw()
