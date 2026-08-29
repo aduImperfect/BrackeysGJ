43c0d209e62e53aa92e1c200b6352495b3e76cfa
Kowshik <saikowsikayyalasomayajula@gmail.com>
2026-08-29 17:58:10 +0530

    spwaning evidenccess

--- brackeys-gj/Scripts/evidencesSpawner.gd
+++ brackeys-gj/Scripts/evidencesSpawner.gd
@@ -3,16 +3,28 @@ extends ScrollContainer
 var evidenceData = preload("res://Resources/evidencesRes.tres")
 var evidenceScene : PackedScene = preload("res://Scenes/evidence_template.tscn")
 @export var holder : Control
+@export var hOffset : float = 150
+@export var vOffset : float = 100
+var spawnedEvidences : Array[Control]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	spawnEvidences()
 
 func spawnEvidences() -> void:
 	for evidenceIndex in  evidenceData.entries.size():
-		var entry = evidenceData.entries[evidenceIndex]
-		entry
-	pass
+		var spawnedEvidence = evidenceScene.instantiate() as EvidenceData
+		var column = evidenceIndex % 2
+		var row = evidenceIndex / 2
+		var spawnPos = global_position + Vector2(
+			column * hOffset,
+			row * vOffset
+		)
+		holder.add_child(spawnedEvidence)
+		spawnedEvidence.name = "Evidence" +  str(spawnedEvidences.size() + 1)
+		spawnedEvidence.global_position = spawnPos
+		spawnedEvidences.append(spawnedEvidence)
+		print(str(evidenceIndex))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
