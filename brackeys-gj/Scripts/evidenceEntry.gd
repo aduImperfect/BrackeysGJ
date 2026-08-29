3ca3ce7cdfd07f73b933177b014d71611cabefc3
Aditya Subramanian <adu101992@gmail.com>
2026-08-29 16:18:05 +0530

    Dummy evidence entries into the resource.

--- brackeys-gj/Scripts/evidenceEntry.gd
+++ brackeys-gj/Scripts/evidenceEntry.gd
@@ -2,6 +2,7 @@ class_name EvidenceEntry
 
 extends Resource
 
+@export var evidence_tex : Texture2D
 @export var evidence_name: String = ""
 @export var characters: Array[String] = []
 @export var location: String = ""
@@ -20,5 +21,4 @@ static func makeEntry(p_name: String, p_characters: Array[String], p_location: S
 static func compareEntries(a: EvidenceEntry, b: EvidenceEntry) -> bool:
 	return a.evidence_name == b.evidence_name \
 		and a.location == b.location \
-		#and a.time == b.time \
 		and a.characters == b.characters \
